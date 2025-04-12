9de8ba1dbbf8b31b8a88ac230687cd442b369228
Derek <derek@frisbeeworld.com>
2025-04-12 16:26:07 +1000

    Emit Kafka messages when getting financial or investment history

--- financial-advisor/src/main/java/functions/Function.java
+++ financial-advisor/src/main/java/functions/Function.java
@@ -1,8 +1,12 @@
 package functions;
 
+import functions.Input;
+import functions.Output;
 import io.quarkus.funqy.Funq;
 import functions.FinancialAdviserService;
 import jakarta.inject.Inject;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 /**
  * Your Function class
@@ -12,6 +16,13 @@ public class Function {
     @Inject
     FinancialAdviserService financialAdviserService;
 
+    // The env var KAFKA_BOOTSTRAP_SERVERS must be set to define the
+    // Kafka bootstrap servers to send to.
+    //
+    @Channel("wealthwise-history")
+    Emitter<String> historyEmitter;
+
+
     /**
      * Use the Quarkus Funqy extension for our function. This function simply echoes its input
      * @param input a Java bean
@@ -20,8 +31,12 @@ public class Function {
     @Funq
     public Output chat(Input input) {
 
-        // Add business logic here
         System.out.println("Getting advice based on: " + input.getMessage());
+
+        // Emit a history message
+        historyEmitter.send("{\"message\": \"" + input.getMessage() + "\"}");
+
+        // Now process the request
         String response = financialAdviserService.chat(input.getMessage());
         return new Output(response);
     }
--- investment-advisor/src/main/java/functions/Function.java
+++ investment-advisor/src/main/java/functions/Function.java
@@ -3,6 +3,10 @@ package functions;
 import io.quarkus.funqy.Funq;
 import functions.InvestmentAdvisorService;
 import jakarta.inject.Inject;
+import functions.Input;
+import functions.Output;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
 
 /**
  * Your Function class
@@ -12,6 +16,12 @@ public class Function {
     @Inject
     InvestmentAdvisorService investmentAdvisorService;
 
+    // The env var KAFKA_BOOTSTRAP_SERVERS must be set to define the
+    // Kafka bootstrap servers to send to.
+    //
+    @Channel("wealthwise-history")
+    Emitter<String> historyEmitter;
+
     /**
      * Use the Quarkus Funqy extension for our function. This function simply echoes its input
      * @param input a Java bean
@@ -19,11 +29,15 @@ public class Function {
      */
     @Funq
     public Output chat(Input input) {
-
-        // Add business logic here
         System.out.println("Getting investment based on: " + input.getMessage());
+
+        // Emit a history message
+        historyEmitter.send("{\"message\": \"" + input.getMessage() + "\"}");
+
         InvestmentAdvisorService.RiskLevel risk = InvestmentAdvisorService.RiskLevel.valueOf(input.getMessage());
         String response = investmentAdvisorService.recommend(risk);
         return new Output(response);
     }
 }
+
+
